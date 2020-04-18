0402fd63b20a9c178b52fd9d05acd9fd9e9f8c84
yechubot <61838900+yechubot@users.noreply.github.com>
2020-04-19 00:33:13 +0900

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,10 @@
+public class Datatype {
+	public static void main(String[] args) {
+		System.out.println(6); //Number
+		System.out.println("six");//String
+		System.out.println("6");//String6
+		System.out.println(6+6);//12
+		System.out.println("6"+"6");//66
+		System.out.println("1111".length());4
+	}
+}
