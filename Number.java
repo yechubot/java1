27c5f08455ddb886bf581b6164fe42ef427940ed
yechubot <61838900+yechubot@users.noreply.github.com>
2020-04-19 22:05:44 +0900

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,18 @@
+package data_and_operation;
+
+public class Number {
+
+	public static void main(String[] args) {
+		//operator 
+		System.out.println(6 + 2);
+		System.out.println(6 - 2);
+		System.out.println(6 * 2);
+		System.out.println(6 / 2);
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI));
+		System.out.println(Math.ceil(Math.PI));
+
+	}
+
+}
---
+++ StringApp.java
@@ -0,0 +1,17 @@
+package data_and_operation;
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");//String
+		System.out.println('H');//Character
+		System.out.println("H");//String
+		
+		System.out.println("Hello"+ "World");
+		System.out.println("Hello \nWorld");//new line
+		System.out.println("Hello \"World\"");//Hello "World"
+
+	}
+
+}
